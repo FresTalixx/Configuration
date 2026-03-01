6b2042c3370a431a19a3cb89942af4c9dd31df98
frest <frest@DESKTOP-DFPD65U>
2026-03-01 15:58:56 +0200

    Add project files.

---
+++ Configuration/Program.cs
@@ -0,0 +1,57 @@
+﻿using Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Binder;
+using Microsoft.Extensions.Configuration.Json;
+using Serilog;
+
+//Консольний застосунок
+//який моніторить директорію (з конфігурації)
+//моніториться як сама директорія так і усі піддерикторії в ньому
+ 
+//Якщо з'явилася нова директорія то пише в лог
+//Якщо видалили директорію яка була, то пише в лог
+//Якщо з'явився новий файл то пише в лог
+//Якщо видалився файл то пише в лог
+ 
+//Для логу використовуємо serilog
+//Інформація про запуск та зупинку програми пишемо в файл log-20260301.log
+//Інформацію про зміни файлів пишемо в файл files-20260301.log
+//Інформацію про зміни директорій пишемо в файл dirs-20260301.log
+
+Serilog.Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .WriteTo.File(
+        path: "logs/log-.txt",
+        rollingInterval: RollingInterval.Day,
+        fileSizeLimitBytes: 10 * 1024 * 1024, // 10 MB
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug
+    )
+    .CreateLogger();
+
+
+IConfiguration congenfiguration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appconfig.json", optional: false, reloadOnChange: true)
+    .Build();
+
+
+//output dir files and sub folders to console
+// get dir path from appsettings.json file
+var dirPath = congenfiguration.GetSection("DirectoryPath").Value;
+
+var files = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+
+foreach (var file in files)
+{
+    Log.Information(file);
+}
+
+var watcher = new directoryWatcher(dirPath);
+
+watcher.WatchDirectory();
+
---
+++ Configuration/directoryWatcher.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration;
+
+internal class directoryWatcher
+{
+    private string _dirPath;
+    public directoryWatcher(string dirPath)
+    {
+        _dirPath = dirPath;
+    }
+    public void WatchDirectory()
+    {
+        var previousFiles = Directory.GetFiles(_dirPath, "*.*", SearchOption.AllDirectories);
+
+        while (true)
+        {
+            var currentFiles = Directory.GetFiles(_dirPath, "*.*", SearchOption.AllDirectories);
+            var newFiles = currentFiles.Except(previousFiles).ToList();
+            var deletedFiles = previousFiles.Except(currentFiles).ToList();
+
+
+            foreach (var file in newFiles)
+            {
+                Log.Information($"New file: {file}");
+            }
+
+            foreach (var file in deletedFiles)
+            {
+                Log.Information($"Deleted file: {file}");
+            }
+
+           previousFiles = currentFiles;
+
+            Thread.Sleep(1000);
+
+        }
+    }
+}
